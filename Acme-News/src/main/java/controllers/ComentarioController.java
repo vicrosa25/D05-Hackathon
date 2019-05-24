116ab0bad0da5c5a1d0d6dc25ac34fa28a6f23f7
Martin <vicmardp2.d02@gmail.com>
2019-05-24 11:19:39 +0200

    Clases comentario arregladas

--- Acme-News/src/main/java/controllers/ComentarioController.java
+++ Acme-News/src/main/java/controllers/ComentarioController.java
@@ -1,33 +1,20 @@
 package controllers;
 
-
-
-
-
-
-
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import services.ComentarioService;
+import services.InformacionService;
+import domain.Comentario;
 import domain.Informacion;
 import domain.Noticia;
-import domain.Comentario;
-
-import services.InformacionService;
-import services.ComentarioService;
-
-
-
-
 
 @Controller
 @RequestMapping("/comentario")
@@ -36,110 +23,77 @@ public class ComentarioController extends AbstractController {
 	//constructor
 	public ComentarioController() {
 		super();
-		
+
 	}
-	
+
 	//Services
-	
+
 	@Autowired
 	private ComentarioService comentarioService;
 
-
-
-	
 	@Autowired
 	private InformacionService informacionService;
-		
 
-	
-	
-		
-		
-		@RequestMapping(value = "/create", method = RequestMethod.GET)
-		public ModelAndView create(@RequestParam int informacionId) {
-			
-			ModelAndView result;
-			Comentario comentario;
-			Informacion informacion= informacionService.findOne(informacionId);
-		
-			comentario = comentarioService.create(informacion);
-			result = new ModelAndView("comentario/edit");
-			result.addObject("comentario", comentario);
-			System.out.println(informacion);
-			System.out.println(informacionId);
-			System.out.println("hola");
-			System.out.println(comentario.getInformacion());
+	/** METHODS **/
+	// Comentar --------------------------------------------------
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public ModelAndView create(@RequestParam int informacionId) {
 
-			return result;
-		}
+		ModelAndView result;
+		Comentario comentario;
+		Informacion informacion= this.informacionService.findOne(informacionId);
+
+		comentario = this.comentarioService.create(informacion);
+		result = new ModelAndView("comentario/edit");
+		result.addObject("comentario", comentario);
+
+		return result;
+	}
 
 
+	// Edit ------------------------------------------------------------------------
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
+	public ModelAndView save(@Valid Comentario comentario, BindingResult binding) {
+		ModelAndView result;
 
-		@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
-		public ModelAndView save(@Valid Comentario comentario, BindingResult binding) {
-			ModelAndView result;
-			
-			
-			
-			if (binding.hasErrors()) {
-				System.out.println(binding.getAllErrors().toString());
-				result = createEditModelAndView(comentario);
-			
-				
-			} else {
-				try {
-					comentarioService.save(comentario);
-					System.out.println("hola2");
-					String redirect=null;
-					if(comentario.getInformacion() instanceof Noticia){
-						redirect="redirect:/noticia/display.do?noticiaId=";
-					}else{
-						 redirect="redirect:/evento/display.do?eventoId=";
-					}
-					
-					
-					redirect=redirect+comentario.getInformacion().getId();
-					result = new ModelAndView(redirect);
-//					result = createEditModelAndView(comentario,"exito");
-					System.out.println(redirect);
-					
-				} catch (Throwable oops) {
-				
-					result = createEditModelAndView(comentario, "comentario.commit.error");
+		if (binding.hasErrors()) {
+			System.out.println(binding.getAllErrors().toString());
+			result = this.createEditModelAndView(comentario);
+		} else {
+			try {
+				this.comentarioService.save(comentario);
+				String redirect=null;
+				if(comentario.getInformacion() instanceof Noticia){
+					redirect="redirect:/noticia/display.do?noticiaId=";
+				}else{
+					redirect="redirect:/evento/display.do?eventoId=";
 				}
-			
+				redirect=redirect+comentario.getInformacion().getId();
+				result = new ModelAndView(redirect);
+				System.out.println(redirect);
+
+			} catch (Throwable oops) {
+
+				result = this.createEditModelAndView(comentario, "comentario.commit.error");
 			}
-			return result;
+
 		}
+		return result;
+	}
 
+	// Ancillary Methods--------------------------
+	protected ModelAndView createEditModelAndView(Comentario comentario) {
+		ModelAndView result;
+		result = this.createEditModelAndView(comentario, null);
+		return result;
+	}
 
-		
-	
-		
-		
+	protected ModelAndView createEditModelAndView(Comentario comentario, String message) {
+		ModelAndView result;
+		result = new ModelAndView("comentario/edit");
+		result.addObject("comentario", comentario);
+		result.addObject("message", message);
 
-		// Ancillary Methods--------------------------
-		protected ModelAndView createEditModelAndView(Comentario comentario) {
-			ModelAndView result;
-			result = createEditModelAndView(comentario, null);
-			return result;
-		}
-
-		protected ModelAndView createEditModelAndView(Comentario comentario, String message) {
-			ModelAndView result;
-			result = new ModelAndView("comentario/edit");
-			result.addObject("comentario", comentario);
-			result.addObject("message", message);
-			
-			
-			
-			return result;
-		}
-		
-		
-
-
-
-
+		return result;
+	}
 }
-
--- Acme-News/src/main/java/services/ComentarioService.java
+++ Acme-News/src/main/java/services/ComentarioService.java
@@ -3,12 +3,13 @@ package services;
 
 
 
+import java.util.Collection;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import org.springframework.util.Assert;
 
 import repositories.ComentarioRepository;
 import domain.Comentario;
@@ -25,10 +26,10 @@ public class ComentarioService {
 	// Managed repository -----------------------------------------------------
 	@Autowired
 	private ComentarioRepository comentarioRepository;
-	
+
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	@Autowired
 	private TasaService tasaService;
 
@@ -40,28 +41,27 @@ public class ComentarioService {
 	}
 
 	// Simple CRUD methods ----------------------------------------------------
-	
+
 	public Comentario create(Informacion informacionId) {
 		Comentario result = new Comentario();
-		result.setUsuario(usuarioService.findByPrincipal());
-		
+		result.setUsuario(this.usuarioService.findByPrincipal());
 		result.setInformacion(informacionId);
 		Date date = new Date();
 		result.setFecha(date);
-		
 
 		return result;
 	}
-	
-	
-
-
-	
 
 	public Comentario findOne(int comentarioID) {
-		Comentario result;
+		Comentario result = this.comentarioRepository.findOne(comentarioID);
+		Assert.notNull(result);
 
-		result = comentarioRepository.findOne(comentarioID);
+		return result;
+	}
+
+	public Collection<Comentario> findAll() {
+		Collection<Comentario> result = this.comentarioRepository.findAll();
+		Assert.notNull(result);
 
 		return result;
 	}
@@ -70,29 +70,18 @@ public class ComentarioService {
 		assert comentario != null;
 		assert comentario.getTitulo() != null;
 		assert comentario.getDescripcion() != null;
-		Comentario result;
+		Comentario result = this.comentarioRepository.save(comentario);
 
-		result = comentarioRepository.save(comentario);
-		
-		Usuario usuarioLogued= usuarioService.findByPrincipal();
+		Usuario usuarioLogued= this.usuarioService.findByPrincipal();
 		Double tasa=0.;
 		if(usuarioLogued.getEstatus().equals(Estatus.PRINCIPIANTE)){
-			tasa=tasaService.findpuntosPrincipiante();
+			tasa=this.tasaService.findpuntosPrincipiante();
 		}else if(usuarioLogued.getEstatus().equals(Estatus.MAESTRO)){
-			tasa=tasaService.findpuntosMaestro();
-		}else tasa=tasaService.findpuntosModerador();
+			tasa=this.tasaService.findpuntosMaestro();
+		}else tasa=this.tasaService.findpuntosModerador();
 
 		usuarioLogued.setPuntos((int) (usuarioLogued.getPuntos()+tasa));
-		
+
 		return result;
 	}
-
-
-	
-	
-	public ComentarioRepository getComentarioRepository() {
-		return comentarioRepository;
-	}
-	
-
 }
