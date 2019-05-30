5a185d206dba7c442113469b91391d3a93ed41e5
Víctor Rosa <vicrosa25@gmail.com>
2019-05-30 13:19:05 +0200

    Add adminChangePrincipalAddTest, Functional Test

--- Acme-News/src/test/java/testSuite/adminChangePrincipalAddTest.java
+++ Acme-News/src/test/java/testSuite/adminChangePrincipalAddTest.java
@@ -1,6 +1,5 @@
+
 package testSuite;
-
-
 
 import javax.transaction.Transactional;
 
@@ -15,78 +14,82 @@ import domain.Banner;
 import services.BannerService;
 import utilities.AbstractTest;
 
-
-@ContextConfiguration(locations = {"classpath:spring/junit.xml"})
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
-public class BannerTest extends AbstractTest {
-	
+public class adminChangePrincipalAddTest extends AbstractTest {
+
 	// System under test ------------------------------------------------------
 	@Autowired
 	private BannerService bannerService;
 
-	//Se cambia el banner autentificado como administrador.
-	@Test
-	public void testCreateBanner(){
-		Banner banner;
-		Banner saved;
-		
-		super.authenticate("admin");	
-		
-		
-		String url="http://www.marketingsgm.es/wp-content/uploads/anuncio-ford.jpg";
-		
-		banner=bannerService.create();
-		banner.setUrl(url);
-		saved = bannerService.save(banner);
-		Assert.isTrue(this.bannerService.findAll().contains(saved));
-		
-		super.unauthenticate();
-		
-	
-	}
-	
-	//se intenta cambiar el banner autentificado como usuario. El resultado esperado es que no se pueda.
-	@Test(expected = IllegalArgumentException.class)
-	public void testChangeBannerNotLogguedAsAdmin(){
-		Banner banner;
-		Banner saved;
-		
-		super.authenticate("usuario4");	
-		
-		
-		String url="http://www.marketingsgm.es/wp-content/uploads/anuncio-ford.jpg";
-		
-		banner=bannerService.create();
-		banner.setUrl(url);
-		saved = bannerService.save(banner);
-		Assert.isTrue(this.bannerService.findAll().contains(saved));
-		
-		super.unauthenticate();
-	
-	}
-	
-	//se intenta cambiar el banner sin autentificarse. El resultado esperado es que no se pueda.
-	@Test(expected = IllegalArgumentException.class)
-	public void testChangeBannerNotLoggued(){
-		Banner banner;
-		Banner saved;
-		
-		super.authenticate(null);	
-		
-		
-		String url="http://www.marketingsgm.es/wp-content/uploads/anuncio-ford.jpg";
-		
-		banner=bannerService.create();
-		banner.setUrl(url);
-		saved = bannerService.save(banner);
-		Assert.isTrue(this.bannerService.findAll().contains(saved));
-		
-		super.unauthenticate();
-	
-	}
-	
-	
 
-	
+	// Test ------------------------------------------------------
+	/*
+	 * An actor who is not authenticated as a admin must be able to: change the principal page adds
+	 * 
+	 * 01- All ok - Positive test
+	 * 02- Actor is not autheticate - Negative test - error
+	 * 03- Actor is authenticated as a user - Negative test - error
+	 * 
+	 */
+
+	@Test
+	public void testCreateBanner() {
+		Banner banner;
+		Banner saved;
+
+		super.authenticate("admin");
+
+		String url = "http://www.marketingsgm.es/wp-content/uploads/anuncio-ford.jpg";
+
+		banner = bannerService.create();
+		banner.setUrl(url);
+		saved = bannerService.save(banner);
+		Assert.isTrue(this.bannerService.findAll().contains(saved));
+
+		super.unauthenticate();
+
+	}
+
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testChangeBannerNotLoggued() {
+		Banner banner;
+		Banner saved;
+
+		super.authenticate(null);
+
+		String url = "http://www.marketingsgm.es/wp-content/uploads/anuncio-ford.jpg";
+
+		banner = bannerService.create();
+		banner.setUrl(url);
+		saved = bannerService.save(banner);
+		Assert.isTrue(this.bannerService.findAll().contains(saved));
+
+		super.unauthenticate();
+
+	}
+
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testChangeBannerNotLogguedAsAdmin() {
+		Banner banner;
+		Banner saved;
+
+		super.authenticate("usuario4");
+
+		String url = "http://www.marketingsgm.es/wp-content/uploads/anuncio-ford.jpg";
+
+		banner = bannerService.create();
+		banner.setUrl(url);
+		saved = bannerService.save(banner);
+		Assert.isTrue(this.bannerService.findAll().contains(saved));
+
+		super.unauthenticate();
+
+	}
+
 }
